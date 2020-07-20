c03b61d4c7fcae9febb0d5f17d13f89f39d69dfe
Henry D. Hills <henry.d.hills@gmail.com>
2020-07-20 09:47:08 -0400

    Added JavaScript and Additional Text

--- js/script.js
+++ js/script.js
@@ -1,11 +1,16 @@
-let isVisible = false
+let isVisible = false;
+let isClicked = false;
 
 $('#slide-down').click(function(){
+    isClicked = true;
     if(isVisible){
         $('#hidden-content').slideUp()
         isVisible = false;
     }else{
         $('#hidden-content').slideDown();
         isVisible = true;
+    }
+    if(isClicked){
+        document.getElementById('slide-down').innerHTML='<h2>additional statistics</h2>';
     }
 });
